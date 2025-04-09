fa425c236f88b97328e104bc73dd7a067c3d3e69
Chris-1010 <122332721@umail.ucc.ie>
2025-04-09 23:40:47 +0200

    Add: Main base UI files

---
+++ raytracer-ui/src/App.tsx
@@ -0,0 +1,11 @@
+import InteractiveRayTracer from './components/InteractiveRayTracer'
+
+function App() {
+    return (
+        <div className="min-h-screen">
+            <InteractiveRayTracer />
+        </div>
+    )
+}
+
+export default App
